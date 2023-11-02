dc7742a7f352e16b867c7aa5a36013a3ed9f5d92
lizamoguchaya <moguchaya-liza2013@yandex.ru>
2023-11-02 15:53:29 +0300

    набросок

--- api.js
+++ api.js
@@ -1,7 +1,9 @@
+import { renderApp, setPosts } from './index.js'
+
 // Замени на свой, чтобы получить независимый от других набор данных.
 // "боевая" версия инстапро лежит в ключе prod
-const personalKey = "prod";
-const baseHost = "https://webdev-hw-api.vercel.app";
+const personalKey = "liza-moguchaya";
+const baseHost = 'https://wedev-api.sky.pro'
 const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
 
 export function getPosts({ token }) {
@@ -65,6 +67,106 @@ export function uploadImage({ file }) {
     method: "POST",
     body: data,
   }).then((response) => {
-    return response.json();
-  });
+    if (response.status === 400) {
+        alert('Размер изображения превышает установленный')
+        renderApp()
+        throw new Error('Слишком большое изображение')
+    } else {
+        return response.json()
+    }
+})
+}
+
+export function addPost({ token, imageUrl }) {
+  const descriptionInputElement = document.getElementById('description')
+  return fetch(postsHost, {
+      method: 'POST',
+      body: JSON.stringify({
+          description: descriptionInputElement.value
+              .replaceAll('&', '&amp;')
+              .replaceAll('<', '&lt;')
+              .replaceAll('>', '&gt;')
+              .replaceAll('"', '&quot;'),
+          imageUrl,
+      }),
+      headers: {
+          Authorization: token,
+      },
+  }).then((response) => {
+      if (response.status === 400) {
+          alert('Заполнены не все поля: фото, описание')
+          throw new Error('Некорректно введены данные')
+      } else {
+          return response.json()
+      }
+  })
+}
+
+export function delPost({ token, postId }) {
+  return fetch(`${postsHost}/${postId}`, {
+      method: 'DELETE',
+      headers: {
+          Authorization: token,
+      },
+  }).then((response) => {
+      if (response.status === 401) {
+          alert('Удалять посты могут только авторизованные пользователи')
+          throw new Error('Нет авторизации')
+      }
+
+      return response.json()
+  })
+}
+
+export function getPostsOneUser({ token, userId }) {
+  return fetch(`${postsHost}/user-posts/${userId}`, {
+      method: 'GET',
+      headers: {
+          Authorization: token,
+      },
+  })
+      .then((response) => {
+          if (response.status === 401) {
+              throw new Error('Нет авторизации')
+          }
+          return response.json()
+      })
+      .then((data) => {
+          setPosts(data.posts)
+          return data.posts
+      })
+      .catch((error) => {
+          alert('Кажется, у вас сломался интернет, попробуйте позже')
+          console.warn(error)
+      })
+}
+export function addLikePost({ token, postId }) {
+  return fetch(`${postsHost}/${postId}/like`, {
+      method: 'POST',
+      headers: {
+          Authorization: token,
+      },
+  }).then((response) => {
+      if (response.status === 401) {
+          alert('Лайкать посты могут только авторизованные пользователи')
+          throw new Error('Нет авторизации')
+      }
+
+      return response.json()
+  })
+}
+export function dislikePost({ token, postId }) {
+  return fetch(`${postsHost}/${postId}/dislike`, {
+      method: 'POST',
+      headers: {
+          Authorization: token,
+      },
+  }).then((response) => {
+      if (response.status === 401) {
+          alert('Лайкать посты могут только авторизованные пользователи')
+          throw new Error('Нет авторизации')
+      }
+
+      return response.json()
+  })
 }
--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -10,6 +10,8 @@ export function renderPostsPageComponent({ appEl }) {
    * TODO: чтобы отформатировать дату создания поста в виде "19 минут назад"
    * можно использовать https://date-fns.org/v2.29.3/docs/formatDistanceToNow
    */
+
+  
   const appHtml = `
               <div class="page-container">
                 <div class="header-container"></div>
